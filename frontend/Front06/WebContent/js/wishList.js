d229e6d6cfe533f0ec6f7c9a565ea09f8ef1af8f
Jaennie <je028770@gmail.com>
2020-05-28 22:27:29 +0900

    [2020-05-28 22:27] update wishList

--- frontend/Front06/WebContent/js/wishList.js
+++ frontend/Front06/WebContent/js/wishList.js
@@ -1,8 +1,8 @@
+
 $(function () {
 	//Storage 받아오기
 	var store = localStorage;
 	var arr = [];
-	var idx = 1001;
 	var price = 0;
 	//없으면 없다는 문구
 	if(store.length == 0){
@@ -12,10 +12,10 @@ $(function () {
 	//있으면 table에 추가
 	else{
 		for(var i=0; i<store.length;i++){
-			arr = store.getItem(String(i+idx)).split(',');
+			arr = store.getItem(store.key(i)).split(',');
 			$('tbody').append('<tr>');
 			$('tbody').append('<td> <img src='+arr[0]+'></td>')
-			$('tbody').append('<td id="key" value='+(i+idx)+'>'+(i+idx)+'</td>');;
+			$('tbody').append('<td id="key" value='+store.key(i)+'>'+store.key(i)+'</td>');;
 			$('tbody').append('<td>'+arr[1]+'</td>');
 			$('tbody').append('<td>'+arr[2]+'</td>');
 			$('tbody').append('<td><button value="delete">삭제</button></td>');
@@ -26,7 +26,16 @@ $(function () {
 	}
 	
 	$('button').on('click',function(){
-		var key = $(this).parent().find('#id').attr('id');
-		alert(key);
+		var key =$(this).parent().prev().prev().prev().html();
+		if(confirm($(this).parent().prev().prev().html()
+				+"가 찜리스트에서 삭제됩니다")){
+			alert('영화가 삭제되었습니다.');
+			store.removeItem(key);
+			location.reload();
+		}
+		else{
+			alert('삭제가 취소되었습니다.');
+			return false;
+		}
 	});
 });
