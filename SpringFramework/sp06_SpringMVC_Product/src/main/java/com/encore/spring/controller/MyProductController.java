b6f4e39dcd33af8172d274f7b73a96c0f9812d3e
Jaeeun Lee <je028770@gmail.com>
2020-08-07 12:09:26 +0900

    [2020-08-07 12:08] Beanfile modularization

--- SpringFramework/sp06_SpringMVC_Product/src/main/java/com/encore/spring/controller/MyProductController.java
+++ SpringFramework/sp06_SpringMVC_Product/src/main/java/com/encore/spring/controller/MyProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.encore.spring.domain.MyProduct;
@@ -17,7 +18,8 @@ public class MyProductController {
 	private MyProductService myProductService;
 	
 	//dispatcherservlet->handlermapping->dispatcherservlet->controller를 거쳐오면서 자동으로 Binding
-	@RequestMapping("myProduct.do")
+	//RequestMethod지워도 됨
+	@RequestMapping(value="myProduct.do", method=RequestMethod.POST)
 	public ModelAndView insert(MyProduct pvo) throws Exception { 
 		System.out.println("폼으로 받은 데이터"+pvo.getId());
 		myProductService.addProduct(pvo);
@@ -26,7 +28,7 @@ public class MyProductController {
 		return new ModelAndView("insert_result","info",pvo);
 	}
 	
-	@RequestMapping("mySearch.do")
+	@RequestMapping(value="mySearch.do", method=RequestMethod.GET)
 	public ModelAndView getProducts(String word) throws Exception { 
 		List<MyProduct> list = myProductService.findByProductName(word);
 		
