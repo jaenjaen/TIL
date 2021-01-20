e00248f508c9f0b4d8f58bc76f9e2b6b2f8ad3d9
jaenjaen <je028770@gmail.com>
2021-01-20 09:33:42 +0900

    [2021-02-20 09:33] JPA 초기 세팅

---
+++ JPA/src/main/java/com/example/jpasample/JpasampleApplication.java
@@ -0,0 +1,14 @@
+package com.example.jpasample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpasampleApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(JpasampleApplication.class, args);
+    }
+
+}
