0eb650d1ce658879336302cd0b932e2068351f77
jaeeunLee <jaeeun.lee@trumpia.com>
2021-01-20 09:25:17 +0900

    [2021-01-20 09:25] JPA 연습 초기 세팅

---
+++ JPA/jpaexample/src/test/java/com/example/jpaexample/JpaexampleApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.jpaexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaexampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
